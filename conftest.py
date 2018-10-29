463b4433c35be6a8dd040fe28323936030def0a6
Chris Hartjes <chartjes@littlehart.net>
2018-10-29 14:26:42 -0400

    Added test for sending a message to the autopush server

--- conftest.py
+++ conftest.py
@@ -4,17 +4,20 @@ import pytest
 import ssl
 
 # Hack because of how SSL certificates are verified by default in Python
-if hasattr(ssl, '_create_unverified_context'):
+if hasattr(ssl, "_create_unverified_context"):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def endpoint():
-    return os.environ['ENDPOINT']
+    return os.environ["ENDPOINT"]
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def bearer_token():
-    return os.environ['BEARER_TOKEN']
+    return os.environ["BEARER_TOKEN"]
 
 
+@pytest.fixture(scope="module")
+def ws_endpoint():
+    return os.environ["WS_ENDPOINT"]
--- tests/test_broadcasts.py
+++ tests/test_broadcasts.py
@@ -1,18 +1,41 @@
 import requests
+import websocket
+from websocket import create_connection
 
 
 def test_broadcast_validation(endpoint, bearer_token):
     url = endpoint + "v1/broadcasts/test/validation"
-    headers = {'Authorization': 'Bearer {}'.format(bearer_token)}
-    resp = requests.put(
-        url=url,
-        data='2018-10-16_01',
-        headers=headers
-    )
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    resp = requests.put(url=url, data="2018-10-16_01", headers=headers)
 
-    '''
+    """
     We're expecting one of two values:
     200 - Validated an existing broadcast
     201 - Created a new broadcast
-    '''
+    """
     assert resp.status_code in (200, 201)
+
+
+def test_sending_messages(ws_endpoint):
+    ws = create_connection(endpoint)
+    ws.send(
+        '{"messageType": "hello", "use_webpush": true, "broadcasts": {"test/validation": "v0"}}'
+    )
+    result = ws.recv()
+    ws.close()
+
+    """
+    Sample response in JSON
+    
+    {
+        "messageType":"hello",
+        "uaid":"92a21288d67b43dea22644a6096430e0",
+        "status":200,
+        "use_webpush":true,
+        "broadcasts":{"test/validation":"2018-10-16_01"}
+    }
+    """
+
+    expected_fields = ["messageType", "uaid", "status", "use_webpush", "broadcasts"]
+    for field in expected_fields:
+        assert field in result
